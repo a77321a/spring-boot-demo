b5379abfc6e441f7adf3b1311b257f9705fe4f0e
a77321a <1817169988@qq.com>
2019-08-20 18:33:02 +0800

    init

---
+++ spring-demo/src/main/java/spring_demo/spring_demo/App.java
@@ -0,0 +1,16 @@
+package spring_demo.spring_demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
