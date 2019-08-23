7e69e3accafad7dbe11039cc2a4000c46bbbfd3a
a77321a <1817169988@qq.com>
2019-08-23 12:09:55 +0800

    热更新 devtools 静态资源文件访问

--- parent0822/child0822/src/main/java/com/xsn/child0822/HelloWorld.java
+++ parent0822/child0822/src/main/java/com/xsn/child0822/HelloWorld.java
@@ -3,11 +3,11 @@ package com.xsn.child0822;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.apache.log4j.Logger;
+//import org.apache.log4j.Logger;
 @RestController
 public class HelloWorld {
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String hello() {
-        return "Hello World1123123";
+        return "Hello";
     }
 }
