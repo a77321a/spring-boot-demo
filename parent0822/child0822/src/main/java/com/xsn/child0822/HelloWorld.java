d8026075f468e52d9b7116468c704d751dc8d84c
a77321a <1817169988@qq.com>
2019-08-26 10:37:11 +0800

    消息转换器 StringHttpMessageConvert 编码

--- parent0822/child0822/src/main/java/com/xsn/child0822/HelloWorld.java
+++ parent0822/child0822/src/main/java/com/xsn/child0822/HelloWorld.java
@@ -1,13 +1,26 @@
 package com.xsn.child0822;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.Charset;
 
 //import org.apache.log4j.Logger;
 @RestController
 public class HelloWorld {
+
+//    定义消息转换器
+//    spring-boot 默认配置了消息转换器
+    @Bean
+    public StringHttpMessageConverter stringHttpMessageConverter(){
+        StringHttpMessageConverter convert = new StringHttpMessageConverter(Charset.forName("utf-8"));
+        return convert;
+    }
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello";
+        return "Hello,你好";
     }
 }
