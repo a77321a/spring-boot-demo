400dc18eae71beadbf954a80d7ae23c065777b73
a77321a <1817169988@qq.com>
2019-08-28 18:18:30 +0800

    mybatis jsp themleaf

--- parent0822/jsp-demo/src/main/java/com/xsn/jspdemo/controll/indexControll.java
+++ parent0822/jsp-demo/src/main/java/com/xsn/jspdemo/controll/indexControll.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class indexControll {
-
-
     @RequestMapping("/index")
     public String indexString(Model model){
         model.addAttribute("name","张三");
-        return "index";
+//        index.jsp
+//        return "index";
+        return "show";
     }
 }
---
+++ parent0822/jsp-demo/src/main/resources/templates/show.ftl
@@ -0,0 +1,8 @@
+<html>
+<head>
+
+</head>
+<body>
+${name}
+</body>
+</html>
