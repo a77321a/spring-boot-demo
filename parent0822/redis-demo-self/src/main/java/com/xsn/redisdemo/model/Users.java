42fbf10a57aeef0b655141466e99d81025ab5fe4
a77321a <1817169988@qq.com>
2019-09-09 16:40:53 +0800

    2

--- parent0822/redis-demo-self/src/main/java/com/xsn/redisdemo/model/Users.java
+++ parent0822/redis-demo-self/src/main/java/com/xsn/redisdemo/model/Users.java
@@ -6,6 +6,15 @@ public class Users implements Serializable {
     private int id;
     private String name;
     private String password;
+    private String sex;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 
     public int getId() {
         return id;
